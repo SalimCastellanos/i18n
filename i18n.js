56ed3b69bdf47f70d5556eef747d98ead25c0d77
Salim Alberto Castellanos Ramirez <salim.castellanos@quipux.com>
2016-07-11 10:43:14 -0500

    i18n v 0.0.1

---
+++ i18n.js
@@ -0,0 +1,60 @@
+/*
+* This is a JavaScript library that can handle different languages (internationalization) in your web applications.
+* 
+* Author  :  Salim A. Castellanos R.
+* Company :  Quipux Innova.
+* Version :  0.0.1
+*
+*/
+
+var $UTIL = {
+        I18N:{}
+};
+
+I18N.config = function(locale){
+    return{
+        getMessage : function(key) {
+            
+            var configNodes = {
+                nodes: [
+                    {
+                        url: "http://localhost:9091",
+                        weight : 2
+                    }
+                ],
+                algoritm: "round_robin",
+                serviceStatus: "none"
+            }
+
+            var url = "/CoreWeb/i18n/messages_"+location+".json";
+            
+            var resources = [
+                {
+                    name   : "getMessages",
+                    url    : url,
+                    method : "GET",
+                    async  : false,
+                    cache  : true
+                }
+            ];
+            
+            
+            var servicio = $UTIL.AJAX.BALANCING(resources, configNodes);
+            
+            var mensajes = "";
+            
+            servicios.resources.getComents({},
+                    function(result){
+                        mensajes = JSON.parse(result);
+                    },
+                    function(err){
+                        alert("entro error: "+err);
+                    }
+                  );
+            
+            return mensajes[key];
+		
+		}
+    
+    }
+}
